067837c5364e81ccdb3e72f15d4ef8d6c19fee4c
wagolemusa <homiemusa@gmail.com>
2021-12-27 12:29:44 +0300

    Set last Ether Price

--- caller/EtherPriceOlacle.sol
+++ caller/EtherPriceOlacle.sol
@@ -14,9 +14,13 @@ contract EthPriceOracle is Ownable {
     emit GetLatestEthPriceEvent(msg.sender, id);
     return id;
   }
-  // Start here
   function setLatestEthPrice(uint256 _ethPrice, address _callerAddress, uint256 _id) public onlyOwner {
     require(pendingRequests[_id], "This request is not in my pending list.");
     delete pendingRequests[_id];
+    // Start here
+    CallerContractInterface callerContractInstance;
+    callerContractInstance = CallerContractInterface(_callerAddress);
+    callerContractInstance.callback(_ethPrice, _id);
+    emit SetLatestEthPriceEvent(_ethPrice, _callerAddress);
   }
 }
