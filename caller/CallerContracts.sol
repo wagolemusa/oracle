042d2574596be42aa311fe5094844d763081b7af
wagolemusa <homiemusa@gmail.com>
2021-12-27 10:28:35 +0300

    callback functiuon

--- caller/CallerContracts.sol
+++ caller/CallerContracts.sol
@@ -1,21 +1,31 @@
 pragma solidity 0.5.0;
-import "./EthPriceOracleInterface.sol";
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
-contract CallerContract is Ownable {
-    EthPriceOracleInterface private oracleInstance;
-    address private oracleAddress;
-    mapping(uint256=>bool) myRequests;
-    event newOracleAddressEvent(address oracleAddress);
-    event ReceivedNewRequestIdEvent(uint256 id);
-    function setOracleInstanceAddress (address _oracleInstanceAddress) public onlyOwner {
-      oracleAddress = _oracleInstanceAddress;
-      oracleInstance = EthPriceOracleInterface(oracleAddress);
-      emit newOracleAddressEvent(oracleAddress);
-    }
-    // Define the `updateEthPrice` function
-    function updateEthPrice() public {
-      uint256 id = oracleInstance.getLatestEthPrice();
-      myRequests[id] = true;
-      emit ReceivedNewRequestIdEvent(id);
-    }
-}
+  import "./EthPriceOracleInterface.sol";
+  import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+  contract CallerContract is Ownable {
+      // 1. Declare ethPrice
+      uint256 private ethPrice;
+      EthPriceOracleInterface private oracleInstance;
+      address private oracleAddress;
+      mapping(uint256=>bool) myRequests;
+      event newOracleAddressEvent(address oracleAddress);
+      event ReceivedNewRequestIdEvent(uint256 id);
+      // 2. Declare PriceUpdatedEvent
+      event PriceUpdatedEvent(uint256 ethPrice, uint256 id);
+      function setOracleInstanceAddress (address _oracleInstanceAddress) public onlyOwner {
+        oracleAddress = _oracleInstanceAddress;
+        oracleInstance = EthPriceOracleInterface(oracleAddress);
+        emit newOracleAddressEvent(oracleAddress);
+      }
+      function updateEthPrice() public {
+        uint256 id = oracleInstance.getLatestEthPrice();
+        myRequests[id] = true;
+        emit ReceivedNewRequestIdEvent(id);
+      }
+      function callback(uint256 _ethPrice, uint256 _id) public {
+        // 3. Continue here
+        require(myRequests[_id], "This request is not in my pending list.");
+        ethPrice = _ethPrice;
+        delete myRequests[_id];
+        emit PriceUpdatedEvent(_ethPrice, _id);
+      }
+  }
